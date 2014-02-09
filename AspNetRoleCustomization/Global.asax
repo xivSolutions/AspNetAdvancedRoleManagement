4b26095a6413d0f1523dc5286b1646ea699b7168
xivsolutions <xivsolutions@gmail.com>
2014-02-09 07:36:13 -0600

    Rename Project

---
+++ AspNetRoleCustomization/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetRoleCustomization.MvcApplication" Language="C#" %>
---
+++ AspNetRoleCustomization/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetRoleCustomization.Startup))]
+namespace AspNetRoleCustomization
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
